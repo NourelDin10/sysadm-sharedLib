4bbff77f364a007cc9b1a88029ae86e046b8a186
Noureldin Mohammed Essa <92956059+NourelDin10@users.noreply.github.com>
2025-12-01 15:11:41 +0200

    Fix image tag variable usage in docker.groovy

--- src/edu/iti/docker.groovy
+++ src/edu/iti/docker.groovy
@@ -1,11 +1,11 @@
 package edu.iti;
 
 def build(imageName, imagetag) {
-    sh "docker build -t $imageName:imagetag ."
+    sh "docker build -t $imageName:$imagetag ."
 }
 def login(username, password) {
     sh"docker login -u $username -p $password"
 }
 def push(imageName, imagetag) {
-    sh "docker push $imageName:imagetag"
+    sh "docker push $imageName:$imagetag"
 }
