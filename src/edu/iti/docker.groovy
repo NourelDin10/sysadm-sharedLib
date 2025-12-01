fb5b5f0b66b15df973321b58e86b608bdfd5ec40
Noureldin Mohammed Essa <92956059+NourelDin10@users.noreply.github.com>
2025-12-01 15:08:50 +0200

    Fix parameter names in Docker functions

--- src/edu/iti/docker.groovy
+++ src/edu/iti/docker.groovy
@@ -1,11 +1,11 @@
 package edu.iti;
 
-def build(imageName, imageName) {
+def build(imageName, imagetag) {
     sh "docker build -t $imageName:imagetag ."
 }
 def login(username, password) {
     sh"docker login -u $username -p $password"
 }
-def push(imageName, imageName) {
+def push(imageName, imagetag) {
     sh "docker push $imageName:imagetag"
 }
