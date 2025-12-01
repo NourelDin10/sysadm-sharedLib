93962bb4c11e98045b0ca8b2ef0e5e8c93769e42
Noureldin Mohammed Essa <92956059+NourelDin10@users.noreply.github.com>
2025-12-01 15:13:00 +0200

    Fix formatting of docker login command

--- src/edu/iti/docker.groovy
+++ src/edu/iti/docker.groovy
@@ -4,7 +4,7 @@ def build(imageName, imagetag) {
     sh "docker build -t $imageName:$imagetag ."
 }
 def login(username, password) {
-    sh"docker login -u $username -p $password"
+    sh "docker login -u $username -p $password"
 }
 def push(imageName, imagetag) {
     sh "docker push $imageName:$imagetag"
